a0416c386f99d68903fbb94817895c98fa54ae5f
Ann-BB <beruashviliani35@gmail.com>
2021-12-04 20:03:43 +0400

    add news section

--- js/main .js
+++ js/main .js
@@ -1,7 +1,12 @@
 let mobilebox=document.getElementById('mobile_box');
 let toggleButton =document.getElementById('togglebutton');
+let toggleLogo =document.getElementById('toggle-Logo');
+
 // let mobHidden=document.getElementById('mobHeader');
 
 togglebutton.addEventListener('click', function(){
     mobilebox.classList.toggle('mob-active');
+})
+toggleLogo.addEventListener('click', function(){
+    mobilebox.classList.remove('mob-active');
 })
