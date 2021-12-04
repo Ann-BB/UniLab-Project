e87ac5142d9c2b3d66a418b9e3a8cdf6d9a6ecd8
Ann-BB <beruashviliani35@gmail.com>
2021-12-04 23:03:40 +0400

    add

--- js/main .js
+++ js/main .js
@@ -1,5 +1,6 @@
 let mobilebox=document.getElementById('mobile_box');
 let toggleButton =document.getElementById('togglebutton');
+let toggeleLogo = document.getElementById('toggle-Logo')
 
 
 togglebutton.addEventListener('click', function(){
